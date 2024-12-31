7c9f96cb56deb7af4dc45d45119976b3a9f76cdf
SwatiBarad <baradswati2001@gmail.com>
2024-12-31 13:28:09 +0530

    change

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,23 @@
 import Userclass from "./Userclass";
 import React from "react";
 
-class About extends React.Component {
-  constructor(props) {
-    super(props);
-    // console.log("parent Constructor");
-  }
-  componentDidMount() {
-    // console.log("parent did mount");
-  }
-  render() {
-    // console.log("parent Render");
-    return (
-      <div>
-        this is About us page
-        <Userclass />
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1>ABOUT US</h1>
+      <p>
+        Swiggy is a new-age consumer-first organization offering an easy-to-use
+        convenience platform, accessible through a unified app.
+      </p>
+      <div className="image-container">
+        <img src="https://swiggy.com/corporate/wp-content/uploads/2024/10/instamart-1-282x300.webp" />
+        <img src="https://www.swiggy.com/corporate/wp-content/uploads/2024/10/food.webp" />
+        <img src="https://www.swiggy.com/corporate/wp-content/uploads/2024/10/dineout-282x300.webp" />
+        <img src="https://www.swiggy.com/corporate/wp-content/uploads/2024/10/genie-1-283x300.webp" />
+        <img src="https://www.swiggy.com/corporate/wp-content/uploads/2024/10/minis-1-283x300.webp" />
       </div>
-    );
-  }
-}
+      <Userclass />
+    </div>
+  );
+};
 export default About;
--- src/components/Userclass.js
+++ src/components/Userclass.js
@@ -1,67 +1,15 @@
-//CLASS BASED COMPONENTS;
-
 import React, { useEffect } from "react";
 import { useEffect } from "react";
-// class Userclass extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       timer: setInterval(() => {
-//         console.log("Swati");
-//       }, 1000),
-//     };
-//     // console.log(this.props.name + "child Constructor");
-//     // }
-//     // async componentDidMount() {
-//     //   const data = await fetch(" https://api.github.com/users/SwatiBarad");
-//     //   const json = await data.json();
-//     //   console.log("json", json);
-
-//     //   this.setState({
-//     //     userInfo: json,
-//     //   });
-
-//     // console.log(this.props.name + "child did mount");
-//   }
-//   componentDidMount() {
-//     this.state.timer;
-//   }
-//   componentWillUnmount() {
-//     clearInterval(this.state.timer);
-//   }
-//   render() {
-//     // console.log(this.props.name + "child Render");
-
-//     return (
-//       <div>
-//         {/* <img src={this.state.userInfo.avatar_url} />
-//         <h1>Name : {this.state.userInfo.login}</h1>
-//         <h1>Location : {this.state.userInfo.location}</h1> */}
-//         {/* <h1>MobileNo. : {mobileNo}</h1> */}
-//         <div>this is class based component</div>
-//       </div>
-//     );
-//   }
-// }
 
 const Userclass = () => {
-  useEffect(() => {
-    timer;
-
-    return () => {
-      clearInterval(timer);
-    };
-  });
-
-  // const mount = () => {
-  //   setInterval(() => {
-  //     console.log("json");
-  //   }, 1000);
-  // };
-  const timer = setInterval(() => {
-    console.log("json");
-  }, 1000);
-  return <div>this is class based component</div>;
+  return (
+    <div className="user-contaier">
+      <div className="data-user">
+        <h1>Get the App now!</h1>
+        <p>For best offers and discounts curated specially for you.</p>
+      </div>
+      <img src="https://www.swiggy.com/corporate/wp-content/uploads/2024/10/app-now-pic-2.webp" />
+    </div>
+  );
 };
 export default Userclass;
